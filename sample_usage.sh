e45e29b5e6dfe850dd4c2e2458077e629d3498a5
Edwin <edwin.orjales@acadiahealthcare.com>
2023-07-22 14:53:24 -0700

    Sample Unix usage

--- sample_usage.sh
+++ sample_usage.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-DIR="/home/ec2-user"
+DIR="/some/dir/containing/images"
 MIN_SIZE="10KB"
 DESKTOP_QUALITY=90
 MOBILE_QUALITY=50
 WORKERS=8
+CHOWN="webapp"
+CHGRP="webapp"
+CHMOD=0775
 
 # make the desktop webps
-/usr/bin/gowebp -d ${DIR} -q ${DESKTOP_QUALITY} -w ${WORKERS} -min-size ${MIN_SIZE} > /var/log/gowebp_desktop.log
+/usr/bin/gowebp -d ${DIR} -q ${DESKTOP_QUALITY} -w ${WORKERS} -min-size ${MIN_SIZE} -chown ${CHOWN} -chgrp ${CHGRP} -chmod ${CHMOD}
 # make the mobile webps
-/usr/bin/gowebp -d ${DIR} -q ${MOBILE_QUALITY} -w ${WORKERS} -append ".mobile" -min-size ${MIN_SIZE} > /var/log/gowebp_mobile.log
-
+/usr/bin/gowebp -d ${DIR} -q ${MOBILE_QUALITY} -w ${WORKERS} -append ".mobile" -min-size ${MIN_SIZE} -chown ${CHOWN} -chgrp ${CHGRP} -chmod ${CHMOD}
